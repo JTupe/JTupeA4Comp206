f2b4217014bebbae8f18fad309ee752635ae500d
JTwizzle <justinetupe@gmail.com>
2017-04-08 16:41:18 -0400

    Update room.c

--- room.c
+++ room.c
@@ -10,7 +10,7 @@ main() {
   
   //create an array, input, of arbritary length. We will store the entire String of the user's input into input array
   //create an array, command, of arbritrary length to store only the command of the user's input
-  char input[80]; command[40];
+  char input[80], command[40];
   
   //reads from stdin for 80 chars and saves into input array
   fgets(input, 80, stdin)
@@ -28,7 +28,7 @@ main() {
     command[i] = "\0";
   }
   
-  char * DROP = "DROP"; PLAY = "PLAY"; EXIT = "EXIT"; REFRESH = "REFRESH";
+  char * DROP = "DROP", PLAY = "PLAY", EXIT = "EXIT", REFRESH = "REFRESH";
   
   if(strncmp(command, DROP, 4) == 0)
   {
@@ -48,9 +48,30 @@ main() {
   else if(strncmp(commmand, REFRESH, 7) == 0)
   {
     //execute code for REFRESH
-  }
-      
+  }  
   else {
     fprintf("There was an error reading the given input. Recall that only: DROP n, PLAY, EXIT or REFRESH are valid inputs.");
+  }
+  
+  //method to read inventory.csv
+  char *getInventory()
+  {
+    FILE *inventory;
+    inventory = fopen("inventory.txt", "r+");
+    int char = 0;
+    int inventory[3];
+    int len = 0;
+    
+    //while(char != EOF && char != '\n')
+    //{
+      //fgets(
+    for(int i = 0; i < 3; i++)
+    {
+      fscanf(inventory, "%[^','], %d, %d, %d", &inventory
+    } 
+  
+  void DROP()
+  {
+    
   }
 }

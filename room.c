7679ee4239809ccd1e9e111479c27f15ba628607
JTwizzle <justinetupe@gmail.com>
2017-04-09 14:35:40 -0400

    Update room.c

--- room.c
+++ room.c
@@ -2,18 +2,21 @@
 #include<stdio.h>
 
 // include the methods of the file //
+void updateInventory(int);
+void updateResources(int);
 
 main(char [] inputCommand, char[] userInventory) {
   char input[200];
+  char command[30];
   char goldDrop[3];
-  int intGoldDrop = 0;
-  int n = atoi(getenv("CONTENT_LENGTH"));
+  int goldDrop = 0;
+  int len = atoi(getenv("CONTENT_LENGTH"));
   
-  fgets(input,n,stdin)
+  fgets(input,len,stdin)
+  {
+    if(input == NULL) { fprintf("There was an error reading the given input. Recall that only: DROP n, PLAY, EXIT or REFRESH are valid inputs."); }
     
-  if(input == NULL) { fprintf("There was an error reading the given input. Recall that only: DROP n, PLAY, EXIT or REFRESH are valid inputs."); }
-    
-  //data received will have the following format: 
+    //data received will have the following format: 
     //file:///C:/Users/JustineTupe/Desktop/index.htm?command=DROP+10&firstname=Mickey&lastname=Mouse
     //reads input until the first '='
     for(int i = 0; i<200 && input!='='; i++);
@@ -35,14 +38,19 @@ main(char [] inputCommand, char[] userInventory) {
       //saves 
       goldDrop[i] = input[i];
     }
+    intGoldDrop = atoi(goldDrop);
+  }
   
   char * DROP = "DROP", PLAY = "PLAY", EXIT = "EXIT", REFRESH = "REFRESH";
   
   if(strncmp(command, DROP, 4) == 0)
   {
-    //execute code for DROP
+    //execute code for DROP i.e update both csv files
+    updateInventory(goldDrop);
+    updateResources(goldDrop);
+    
     // NEED TO PUT THE FILENAME OF THE HTML WEBSITE FILE
-    FILE *website = fopen("website.txt", r+);
+    FILE *website = fopen("room.html", r+);
     int ch;
     
     printf("Content-Type:text/html\n\n");
@@ -57,65 +65,75 @@ main(char [] inputCommand, char[] userInventory) {
     {
       while((ch=fgetc(website) != EOF) 
             { 
-              putchar(ch); 
-              fclose(website); 
+              putchar(ch);
             }
      }
+     fclose(website); 
      printf("</html>");
-     return 0;
   }
   
   else if(strncmp(command, PLAY, 4) == 0)
   {
     //execute code for PLAY
+    FILE *game = fopen("game.c", r);
+    int ch;
+    
+    printf("Content-Type:text/html\n\n");
+    printf("<html>");
+    
+    if(game == NULL);
+    {
+      printf("<head><title>ERROR</title></head>");
+      printf("<body><p>Unable to open file!</p></body>");
+    }
+    else
+    {
+      while((ch=fgetc(game) != EOF) 
+            { 
+              putchar(ch);
+            }
+     }
+     fclose(game); 
+     printf("</html>");
   }
   
   else if(strncmp(command, EXIT, 4) == 0)
   {
     //execute code for EXIT
+    //must rewrite the html page with a sorry to see you go screen
+    //must update the resources file so that the room is unoccupied (resources[3] = 0)
   }
   
   else if(strncmp(commmand, REFRESH, 7) == 0)
   {
-    //execute code for REFRESH
+    FILE *website = fopen("room.html", r+);
+    int ch;
+    
+    printf("Content-Type:text/html\n\n");
+    printf("<html>");
+    
+    if(website == NULL);
+    {
+      printf("<head><title>ERROR</title></head>");
+      printf("<body><p>Unable to open file!</p></body>");
+    }
+    else
+    {
+      while((ch=fgetc(website) != EOF) 
+            { 
+              putchar(ch);
+            }
+     }
+     fclose(website); 
+     printf("</html>");
   }  
   else {
     fprintf("There was an error reading the given input. Recall that only: DROP n, PLAY, EXIT or REFRESH are valid inputs.");
   }
   
-  //method to read inventory.csv
-  char *getInventory()
+  //method to update resources.csv
+  void updateResources(int drop)
   {
-    int inventory[3];
-    
-    while(!feof(fileInventory))
-    {
-      //reads fileInventory, ignores commas, saves each int so that manna=inventory[0], gold=inventory[1];
-      sscanf(fileInventory, "%[^','], %d, %d", inventory[i])
-      {
-        i++;
-      }
-    }
-  }
-  
-  void DROP()
-  {
-    char inventory[2];
-    intGoldDrop = atoi(goldDrop);
-    // calculate how much manna we need to add to the players inventory
-    intMannaAdd = intGoldDrop / 2;
-    
-    //SOMEHOW Add manna to the inventory file. Deduct gold from inventory file.
-    FILE *fileInventory;
-    fileInventory = fopen("inventory.txt", "r+");
-    if(fileInventory == NULL) { perror("Error opening inventory file"); return(-1); }
-    
-    fscanf(fileInventory, "%d,%[^,],%d,%[^,]", &inventory[0], &inventory[1]);
-    //manna = inventory[0], gold = inventory[1]
-    inventory[0] = inventory[0] + intMannaAdd;
-    inventory[1] = inventory[1] - intGoldDrop;
-    //NEED TO WRITE TO THE FILE
-    
     //change gold variable in the resource file to
     char resources[3];
     FILE *fileResources;
@@ -124,7 +142,31 @@ main(char [] inputCommand, char[] userInventory) {
     
     fscanf(fileResources, "%d,%[^,],%d,%[^,],%d,%[^,]", &resources[0], &resources[1], &resources[2]);
     //manna, gold, occupied
-    resources[1] = resources[1] + intGoldDrop;
+    resources[1] = resources[1] + drop;
     //NEED TO WRITE TO THE FILE
-  }
+
+    char comma = ",";
+    fprintf(fileResources, "%d, %c, %d, %c", resources[0], comma, resources[1], comma, resources[2]);
+   }
+            
+   void updateInventory(int drop)
+   {
+     char inventory[2];
+     
+     if(drop != 0) { int manAdd = drop / 2; }
+     else { int manAdd = 0; }
+    
+     //read and write to the inventory file. Deduct gold from inventory file.
+     FILE *fileInventory;
+     fileInventory = fopen("inventory.txt", "r+");
+     if(fileInventory == NULL) { perror("Error opening inventory file"); return(-1); }
+    
+     fscanf(fileInventory, "%d,%[^,],%d,%[^,]", &inventory[0], &inventory[1]);
+     //manna = inventory[0], gold = inventory[1]
+     inventory[0] = inventory[0] + manAdd;
+     inventory[1] = inventory[1] - drop;
+     
+     char comma = ",";
+     fprintf(fileInventory, "%d, %c, %d, %c", inventory[0], comma, inventory[1], comma);
+   }
 }

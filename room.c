f40f190f62878f17bb60f32985ef9ac8e195d743
JTwizzle <justinetupe@gmail.com>
2017-04-12 22:40:12 -0400

    Update room.c

--- room.c
+++ room.c
@@ -6,214 +6,158 @@ void updateInventory(int);
 void updateResources(int);
 void exitUpdateInventory(void);
 
-main(char inputCommand[], char userInventory[]) {
-  char input[200];
-  char command[30];
-  char goldDropArray[3];
-  int goldDrop = 0;
-  int len = atoi(getenv("CONTENT_LENGTH"));
-  char c;
-  
-  while((c = getchar()) != EOF && c < len){
-  fgets(input,len,stdin);
-  {
-    if(input == NULL) 
-	{ 
-		FILE *website = fopen(room.html, r+);
-		fprintf(website, "%s", "There was an error reading the given input. Recall that only: DROP n, PLAY, EXIT or REFRESH are valid inputs."); 
-		fclose(website);
+main(char inputCommand[]) {
+	char input[200], command[200];
+	char inventoryManna[200], inventoryGold[200], goldDropArray[200];
+	int len = atoi(getenv("CONTENT_LENGTH"));
+	char c;
+	
+	// to read how many resources are in the room
+	int rmMan, rmGold, occ;
+	FILE *fileResources = fopen("resources.csv", "r+");
+	fscanf(fileResources, "%d,%[^,],%d,%[^,],%d", &rmMan, &rmGold, &occ);
+	// we will fclose(fileResources) at the very end of this file.
+	
+	while((c = getchar()) != EOF && c < len){
+		fgets(input,len+1,stdin);
 	}
+		
+		if(input == NULL) 
+		{ 
+			printf("Content-Type:text/html\n\n");
+			printf("<html><head>There was an error reading the given input. Recall that only: DROP n, PLAY, EXIT or REFRESH are valid inputs.</head></html>");
+		}
     
-    //data received will have the following format: 
-    //file:///C:/Users/JustineTupe/Desktop/index.htm?command=DROP+10
-    //reads input until the first '='
-    for(int i = 0; input[i]!='='; i++);{
-    //reads the String until first '&'
-    for(int j = i; input[i]!='+'; i++)
-    {
-      //saves the command into command array
-      command[j] = input[i];
-    }
-    }
-    //turns command array into a string by adding a CR
-    command[j] = "\0";
-
-    //read user input to extract the number of gold pieces to drop
-    //reads input until first '+'
-    for(int k = 0; input[k]!='+'; k++);{
-    //reads input until next '&'
-    for(int m = k; input[m]!='&'; k++)
-    {
-      //saves 
-      goldDropArray[j] = input[i];
-    }
-    }
-	  goldDropArray[j] = "\0";
-    goldDrop = atoi(goldDropArray);
-  }
-}
-
-  char * DROP = "DROP", PLAY = "PLAY", EXIT = "EXIT", REFRESH = "REFRESH";
-  
-  if(strncmp(command, DROP, 4) == 0)
+		//data received will have the following format: 
+    		//file:///C:/Users/JustineTupe/Desktop/index.htm?command=DROP+10&inventory=10,10
+    		//reads input until the first '='
+		for(int i = 0; input[i]!='='; i++);
+		{
+			//reads the String until first '&'
+			for(int j = i; input[i]!='+'; i++)
+			{
+				//saves the command into command array
+				command[j] = input[i];
+			}
+		}
+		//turns command array into a string by adding a CR
+		command[j] = "\0";
+		
+		//read user input to extract the number of gold pieces to drop
+		//reads input until first '+'
+		for(int k = 0; input[k]!='+'; k++);
+		{
+			//reads input until next '&'
+			for(int m = k; input[m]!='&'; k++)
+			{
+				//saves 
+				goldDropArray[j] = input[i];
+			}
+		}
+		goldDropArray[j] = "\0";  
+		int goldDrop = atoi(goldDropArray);
+		
+		// read hidden tag for the inventoryManna
+		for(int i = 0; i < len+1 && input[i]!='='; i++); 
+		{
+			for(int j = i; i<len+1 && input[i]!='%'; i++)
+			{
+				inventoryManna[j] = input[i];
+			}
+		}
+		inventory[j]='\0';
+		// read hidden tag for the inventoryGold
+		
+		int playMan = atoi(inventoryManna);
+		int playGold = atoi(inventoryGold);
+		
+		
+  // test for which command the user input. Run the code accordingly
+  if(strncmp(command, "DROP", 4) == 0)
   {
-    //execute code for DROP i.e update both csv files
-    updateInventory(goldDrop);
-    updateResources(goldDrop);
-    
-    // NEED TO PUT THE FILENAME OF THE HTML WEBSITE FILE
-    FILE *website = fopen("room.html", r+);
-    int ch;
-    
-    if(website == NULL);
-    {
-      printf("%s", "<head><title>ERROR</title></head>");
-      printf("%s", "<body><p>Unable to open file!</p></body>");
-    }
-    else
-    {
-      while((ch=fgetc(website) != EOF) 
-	  { 
-		  printf("%c", ch);
-	  }
-     }
-     fclose(website); 
+	  int manAdd = goldDrop / 2;
+	  
+	  playGold = playGold - goldDrop;
+	  playMan = playMan + manAdd;
+	  rmGold = rmGold + goldDrop;
+
   }
   
-  else if(strncmp(command, PLAY, 4) == 0)
+  else if(strncmp(command, "PLAY", 4) == 0)
   {
     //execute code for PLAY
-    FILE *challenge = fopen("challenge.c", r);
+    FILE *challenge = fopen("../challenge.c", r);
     int ch;
 	  
-	  while((ch=fgetc(challenge) != EOF) { 
-			printf("%c", ch);
-		}
+	while((ch=fgetc(challenge) != EOF) 
+	{ 
+		printf("%c", ch);
+	}
 		
     fclose(challenge);
   }
   
-  else if(strncmp(command, EXIT, 4) == 0)
+  else if(strncmp(command, "EXIT", 4) == 0)
   {
-	  FILE *website = fopen("room.html", r+);
-    //execute code for EXIT
-    //must update the resources file so that the room is unoccupied (resources[3] = 0)
-	  ExitUpdateInventory();
-	  printf("%s", "<html>");
-	  printf("%s", "<head><title>Bye! Au Revoir!</title><h1>Aw shucks! Leaving already? Prison isn't for everyone. Come back later, if you dare.</h1>");
-	  printf("%s", "<h2>Quel dommamge! Vous-partez deja? Je comprends. La prison ce n'est pas pour tout le monde. Reviennez plutard, si vous l'osez faire.");
-	  printf("%s", "</head></html>");
-	  fclose(website);
-  }
-  
-  else if(strncmp(commmand, REFRESH, 7) == 0)
-  {
-    FILE *website = fopen("room.html", r+);
-    int ch;  
-  
-    while((ch=fgetc(website) != EOF) 
-            { 
-		    printf("%c", ch);
-            }
-     }
-     fclose(website); 
-	    
-  }  
-  else { //the user input an invalid command
-	FILE *website = fopen("room.html", r+);
-	  printf("%s", "<html>");
-	  printf(website, "%s", "<head>There was an error reading the given input. Recall that only: DROP n, PLAY, EXIT or REFRESH are valid inputs.</head>");
- 	  printf("%s", "</html>");
-	  fclose(website); 
-  }
-  
-  // we could alt create source files with the methods updateResources and updateInventory and compile it into room.o
-  gcc -g -Wall room.c -o room;
-            
-  //method to update resources.csv
-  void updateResources(int drop)
-  {
-    //change gold variable in the resource file to
-    int resources[3];
-    FILE *fileResources;
-    fileResources = fopen("resources.csv", "r+");
-    if(fileResources == NULL) { perror("Error opening resource file"); return(-1); }
-    
-    fscanf(fileResources, "%d,%[^,],%d,%[^,],%d", &resources[0], &resources[1], &resources[2]);
-    //manna, gold, occupied
-    resources[1] = resources[1] + drop;
-    //NEED TO WRITE TO THE FILE
-
-    char comma = ",";
-    fprintf(fileResources, "%d, %c, %d, %c, %d", resources[0], comma, resources[1], comma, resources[2]);
+	  //must update the variables in the resources file
+	  rmMan = rmMan + playMan;
+	  rmGold = rmGold + playGold;
+	  occ = 0;
 	  
-	  fclose(fileResources);
-   }
-          
-   void updateInventory(int drop)
-   {
-     int inventory[2];
-     
-     if(drop != 0) { int manAdd = drop / 2; }
-     else { int manAdd = 0; }
-    
-     //read and write to the inventory file. Deduct gold from inventory file.
-     FILE *fileInventory;
-     fileInventory = fopen("inventory.csv", "r+");
-     if(fileInventory == NULL) { perror("Error opening inventory file"); return(-1); }
-    
-     fscanf(fileInventory, "%d,%[^,],%d,%[^,]", &inventory[0], &inventory[1]);
-     //manna = inventory[0], gold = inventory[1]
-     inventory[0] = inventory[0] + manAdd;
-     inventory[1] = inventory[1] - drop;
-     
-     char comma = ",";
-     fprintf(fileInventory, "%d, %c, %d, %c", inventory[0], comma, inventory[1], comma);
-	   fclose(fileInventory);
-   }
-
-    void exitUpdateInventory(void)
-    {
-	// open, read and save the players inventory
-	int inventory[2];
+	  //reprint page with sorry to see you go
+	  printf("Content-Type:text/html\n\n");
+	  printf("<html>");
+	  printf("<head><title>Bye! Au Revoir!</title><h1>Aw shucks! Leaving already? Prison isn't for everyone. Come back later, if you dare.</h1>");
+	  printf("<h2>Quel dommamge! Vous-partez deja? Je comprends. La prison ce n'est pas pour tout le monde. Reviennez plutard, si vous l'osez faire.</h2>");
+	  printf("</head></html>");
+  }
+  
+  else if(strncmp(commmand, "REFRESH", 7) == 0)
+  {
+	  printf("Content-Type:text/html\n\n");
+	  printf("<html><head><center><title> The Room</title></head><body bgcolor=\"grey\"><h1>Welcome to your room / Bienvenue dans votre chambre</h1><p><i>An eternity of terror awaits you / Une eternite de terreur vous attend</i></p>");
+	  printf("<center><p><img src=\" https://aos.iacpublishinglabs.com/question/aq/700px-394px/what-is-the-size-of-a-prison-cell_a3b78e5d-2784-49d5-bf43-44bf4fbfd908.jpg?domain=cx.aos.ask.com\"></p>");	
+	  printf("<form action=\"https://www.cs.mcgill.ca/~aturim/cgi-bin/transporter.py\" method=\"post\" stlye=\"display\": inLine\"><input type=\"hidden\" name=\"inventory\" value=\"%d,%d\">",playMan,playGold);
+	  printf("<input type=\"hidden\" name=\"URL\" value=\"https://www.cs.mcgill.ca/~aturim/cgi-bin/room.cgi\"><input type=\"submit\" value=\"North\"></form><br>");				
+	  printf("<form action=\"https://www.cs.mcgill.ca/~aturim/cgi-bin/transporter.py\" method=\"post\" style=\"display: inLine\"><input type=\"hidden\" name=\"inventory\" value=\"%d,%d\">",playMan,playGold);
+	  printf("<input type=\"hidden\" name=\"URL\" value=\"https://www.cs.mcgill.ca/~aturim/cgi-bin/room.cgi\"><input type=\"submit\" name=\"button\" value=\"West\"");
+	  printf("</form><form action=\"https://www.cs.mcgill.ca/~aturim/cgi-bin/transporter.py\" method=\"post\" style=\"display: inLine\"><input type=\"hidden\" name=\"inventory\" value=\"%d,%d\">",playMan,playGold");
+	  printf("<input type=\"hidden\" name=\"URL\" value=\"https://www.cs.mcgill.ca/~aturim/room.cgi\"><input type=\"submit\" name=\"button\" value=\"East\"></form><br>");	
+	  printf("<form action=\"https://www.cs.mcgill.ca/~aturim/cgi-bin/transporter.py\" method=\"post\" style=\"display: inLine\"><input type=\"hidden\" name=\"inventory\" value=\"%d,%d\">",playMan,playGold");
+	  printf("<input type=\"hidden\" name=\"URL\" value=\"https://www.cs.mcgill.ca/~aturim/room.cgi\"><input type=\"submit\" name=\"button\" value=\"South\"></form><br>");
+	  printf("<p>Type in Command. Valid actions are PLAY, Drop n, EXIT, REFRESH</p><div><form action=\"https://www.cs.mcgill.ca/~aturim/room.cgi\"method=\"post\" id=\"textbox\" style=\"display: inline\"></div><div>");
+	  printf("<label>COMMAND:<input name=\"command\" size=\"20\"></label></div><input type=\"hidden\" value=\"%d,%d\">",playMan,playGold");
+	  printf("name=\"inventory\"></form><div><b><u>Current Player Inventory</u></b></div><b>MANNA: </b> %d<b>GOLD:</b>%d</center></body></html>",playMan, playGold);
+  }  
+  else { //the user input an invalid command. Reprint the page
+	  printf("Content-Type:text/html\n\n");
+	  printf("<html><head><center><title> The Room</title></head><body bgcolor=\"grey\"><h1>Welcome to your room / Bienvenue dans votre chambre</h1><p><i>An eternity of terror awaits you / Une eternite de terreur vous attend</i></p>");
+	  printf("<center><p><img src=\" https://aos.iacpublishinglabs.com/question/aq/700px-394px/what-is-the-size-of-a-prison-cell_a3b78e5d-2784-49d5-bf43-44bf4fbfd908.jpg?domain=cx.aos.ask.com\"></p>");	
+	  printf("<form action=\"https://www.cs.mcgill.ca/~aturim/cgi-bin/transporter.py\" method=\"post\" stlye=\"display\": inLine\"><input type=\"hidden\" name=\"inventory\" value=\"%d,%d\">",playMan,playGold);
+	  printf("<input type=\"hidden\" name=\"URL\" value=\"https://www.cs.mcgill.ca/~aturim/cgi-bin/room.cgi\"><input type=\"submit\" value=\"North\"></form><br>");				
+	  printf("<form action=\"https://www.cs.mcgill.ca/~aturim/cgi-bin/transporter.py\" method=\"post\" style=\"display: inLine\"><input type=\"hidden\" name=\"inventory\" value=\"%d,%d\">",playMan,playGold);
+	  printf("<input type=\"hidden\" name=\"URL\" value=\"https://www.cs.mcgill.ca/~aturim/cgi-bin/room.cgi\"><input type=\"submit\" name=\"button\" value=\"West\"");
+	  printf("</form><form action=\"https://www.cs.mcgill.ca/~aturim/cgi-bin/transporter.py\" method=\"post\" style=\"display: inLine\"><input type=\"hidden\" name=\"inventory\" value=\"%d,%d\">",playMan,playGold");
+	  printf("<input type=\"hidden\" name=\"URL\" value=\"https://www.cs.mcgill.ca/~aturim/room.cgi\"><input type=\"submit\" name=\"button\" value=\"East\"></form><br>");	
+	  printf("<form action=\"https://www.cs.mcgill.ca/~aturim/cgi-bin/transporter.py\" method=\"post\" style=\"display: inLine\"><input type=\"hidden\" name=\"inventory\" value=\"%d,%d\">",playMan,playGold");
+	  printf("<input type=\"hidden\" name=\"URL\" value=\"https://www.cs.mcgill.ca/~aturim/room.cgi\"><input type=\"submit\" name=\"button\" value=\"South\"></form><br>");
+	  printf("<p>Type in Command. Valid actions are PLAY, Drop n, EXIT, REFRESH</p><div><form action=\"https://www.cs.mcgill.ca/~aturim/room.cgi\"method=\"post\" id=\"textbox\" style=\"display: inline\"></div><div>");
+	  printf("<label>COMMAND:<input name=\"command\" size=\"20\"></label></div><input type=\"hidden\" value=\"%d,%d\">",playMan,playGold");
+	  printf("name=\"inventory\"></form><div><b><u>Current Player Inventory</u></b></div><b>MANNA: </b> %d<b>GOLD:</b>%d</center></body></html>",playMan, playGold);
+  }
+		 
+	// update the resource file
+	char comma = ",";
+	fprintf(fileResources, "%d, %c, %d, %c, %d", rmMan, comma, rmGold, comma, occ);
+	fclose(fileResources);
+	
+	//update the inventory file
 	FILE *fileInventory;
-     	fileInventory = fopen("inventory.csv", "r+");
-     	
-	//send error if inventory file cannot be opened
-	if(fileInventory == NULL) 
-	{ 
-		perror("Error opening inventory file"); 
-		fclose(fileInventory); 
-		return(-1); 
-	}
-	    
-	else {
-		//read the player's inventory
-		fscanf(fileInventory, "%d,%[^,],%d,%[^,]", &inventory[0], &inventory[1]); 
-		
-		//open the room's resources
-		int resources[3];
-    		FILE *fileResources;
-    		fileResources = fopen("resources.csv", "r+");
-		
-		//send error if the resource file cannot be opened
-    		if(fileResources == NULL) { perror("Error opening resource file"); fclose(fileResources); return(-1); }
-    		
-    		else 
-		{ 
-			//read the room's resources
-			fscanf(fileResources, "%d,%[^,],%d,%[^,],%d", &resources[0], &resources[1], &resources[2]);
-			//update the room's resources. Resources: manna, gold, occupied. Inventory: manna, gold
-			resource[0] = resource[0] + inventory[0]; //updates the manna
-			resource[1] = resource[1] + inventory[1]; //updates the gold
-			resource[2] = 0; //updates room as unoccupied
-			
-			//write to resource file to update it
-    			char comma = ",";
-    			fprintf(fileResources, "%d, %c, %d, %c, %d", resources[0], comma, resources[1], comma, resources[2]);
-	    	}
-	    }
-	    fclose(fileInventory);
-	    fclose(fileResources);
-    }
+	fileInventory = fopen("inventory.csv", "r+");
+	char comma = ",";
+     	fprintf(fileInventory, "%d, %c, %d, %c", playMan, comma, playGold, comma);
+	fclose(fileInventory);
+		 
+	// we could alt create source files with the methods updateResources and updateInventory and compile it into room.o
+	gcc -g -Wall room.c -o room;
 }

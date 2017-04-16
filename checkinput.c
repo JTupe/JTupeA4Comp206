ed11067726cf1c9ea943858f94bfb8a18dc81289
JTwizzle <justinetupe@gmail.com>
2017-04-15 20:31:22 -0400

    Update checkinput.c

--- checkinput.c
+++ checkinput.c
@@ -6,13 +6,14 @@ int main(void) {
   
   int len = atoi(getenv("CONTENT_LENGTH"));
   int i;
+  char input [200];
   fgets(input,len+1,stdin);
   
   printf("Content-Type:text/html\n\n");
   printf("<html>");
     
   while( input[i] < (len+1)){
-    printf("%d", i);
+    printf("%d", input[i]);
     i++;
   }
   
